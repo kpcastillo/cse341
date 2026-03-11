fd7df559dd3a8eb1727009ab74838734d02fdd89
kpcastillo <krn_iveth09@hotmail.com>
2026-03-11 02:04:24 -0700

    changed controllers

--- backend/controllers/contacts.ts
+++ backend/controllers/contacts.ts
@@ -1,19 +1,19 @@
 import type { Request, Response, NextFunction } from 'express';
 import { getDb } from '../db/connection.js';
+//import { ObjectId } from 'mongodb';
 
 const getContactById = async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
-  const result = getDb()?.collection('contacts').find({});
-  result?.toArray().then((lists) =>{
+  const result = getDb()?.collection('contacts').find({ _id: id });
+  result?.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]?._id);
+    res.status(200).json(lists[0]);
   });
 };
 const getAll = async (req: Request, res: Response, next: NextFunction) => {
   const result = getDb()?.collection('contacts').find();
   result?.toArray().then((lists) =>{
     res.setHeader('Content-Type', 'application/json');
-    console.log('contacts found:', lists.length);
     res.status(200).json(lists);
   });
 };
